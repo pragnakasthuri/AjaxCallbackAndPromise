25a7f329ba5a96f9d4dba671c8f99a6227dae855
pragnakasthruri <kpragna25@gmail.com>
2022-04-17 18:41:58 +0530

    Adding the js code for method callback

--- JavaScript/ajax.js
+++ JavaScript/ajax.js
@@ -1,12 +1,43 @@
-function showTime() {
-    const date = new Date();
-        return date.getHours() + "Hrs:" +date.getMinutes() + "Mins:" +date.getSeconds()+ "Secs";
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function showTime(){
+    const date= new Date();
+    return date.getHours()+" Hrs "+date.getMinutes()+" mins "+date.getSeconds()+" s."
+}
+const getURL = "http://127.0.0.1:3000/employees/1";
+function getUserDetails(data){
+    console.log("Get User Data at: " + showTime() + " data: " +data)
+ } 
+makeAJAXCall("GET", getURL, getUserDetails, true);
+console.log("Made GET AJAX Call to Server at "+showTime());
+const deleteURL = "http://localhost:3000/employees/4";
+function userDeleted(data){
+   console.log("User Deleted at: " + showTime() + " data: "+data)
+}
+makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+console.log("Made DELETE AJAX Call to Server at "+showTime());
+
+const postURL = "http://localhost:3000/employees";
+const emplData = {"name": "Kavya","salary": "3000"};
+function userAdded(data){
+   console.log("User Added at: " + showTime() + " data: "+data)
+}
+makeAJAXCall("POST", postURL, userAdded, true, emplData);
+console.log("Made POST AJAX Call to Server at "+showTime());
+function makeAJAXCall(methodType, url, callback, async = true, data = null) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){ 
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 201){
+                callback(xhr.responseText);
+            } else if (xhr.status >= 400) {
+                console.log("Handle 400 Client Error or 500 Server Error at: "+showTime());
+            }
+        }
     }
-
-function showSessionExpire() {
-    console.log("Activity-B: Your session expired at "+showTime());
-}    
-
-console.log("Activity-A: Trigerring Activity-B at "+showTime());
-setTimeout(showSessionExpire, 5000);
-console.log("Activity-A: Triggered Activity-B at " +showTime()+ " will execute after 5 seconds");
+    xhr.open(methodType, url, async);
+    if (data) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+     } else xhr.send();
+    console.log(methodType+ " request sent to the server at "+showTime());
+}
